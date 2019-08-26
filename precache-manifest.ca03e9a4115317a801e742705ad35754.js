966fb1b27fbb96543ea6acb6db233971edbafda1
Jordan WIlliams <jordan@Jordans-MacBook-Pro.local>
2019-08-26 12:06:55 +0100

    Updates

---
+++ precache-manifest.ca03e9a4115317a801e742705ad35754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6bff29a05863b32109a7d937de72f5",
+    "url": "isonoe18.github.io/suhpa-dev/index.html"
+  },
+  {
+    "revision": "8ae0a74b87567e25c49c",
+    "url": "isonoe18.github.io/suhpa-dev/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0b33e347aec80dff989b",
+    "url": "isonoe18.github.io/suhpa-dev/static/js/2.951719c3.chunk.js"
+  },
+  {
+    "revision": "8ae0a74b87567e25c49c",
+    "url": "isonoe18.github.io/suhpa-dev/static/js/main.f5f8f5d5.chunk.js"
+  },
+  {
+    "revision": "807f552a72108cd6b249",
+    "url": "isonoe18.github.io/suhpa-dev/static/js/runtime~main.7750fecc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "isonoe18.github.io/suhpa-dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
