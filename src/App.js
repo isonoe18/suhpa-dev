273b42c007b84d34b024e619507649901fff82d4
Jordan WIlliams <jordan@Jordans-MacBook-Pro.local>
2019-08-28 13:53:50 +0100

    v0.4.1: Committee Added

--- src/App.js
+++ src/App.js
@@ -5,40 +5,128 @@ import './index.css'
 
 class App extends Component {
   state = {
-    committee: [{
-      "id": 1,
-      "image": "./img/committee/pran.jpg",
-      "name" : "Prannay Roy",
-      "job" : "Treasurer",
-      "quote": "Prannay is a second year Aeronautics and Astronautics student. He has been in charge of the wing design since the creation of the society, and was recently made treasurer."
-    },
+    committee: [
+      {
+        "id": 0,
+        "image": "./img/committee/default-user.jpg",
+        "name": "Template",
+        "jobs": [{
+          "id": 1,
+          "job": "template Job"
+        }],
+        "quote": "Description"
+      },
+      {
+        "id": 3,
+        "image": "./img/committee/tat.jpg",
+        "name": "Tatjana Mandil",
+        "jobs": [{
+          "id": 1,
+          "job": "Presdient"
+        }],
+        "quote": "Tatjana is a second year Aeronautics and Astronautics student, with a focus on propulsion. She is one of the founding members of SUHPA"
+      },
+      {
+        "id": 4,
+        "image": "./img/committee/charles.jpg",
+        "name": "Charles Dhenin",
+        "jobs": [{
+          "id": 1,
+          "job": "Vice-President"
+        },
+        {
+          "id": 2,
+          "job": "Chief Engineer"
+        }],
+        "quote": "Charles is a former engineering foundation year student, with a passion for aircraft design, additive manufacturing and propulsion. He joined SUHPA in September 2018"
+      },
     {
       "id": 2,
       "image": "./img/committee/jordan.jpg",
       "name": "Jordan Williams",
-      "job": "Welfare Officer",
+      "jobs": [{
+        "id" : 1,
+        "job" : "Welfare Officer"
+      },
+      {
+        "id" : 2,
+        "job" : "Health & Safety Officer"
+      }],
       "quote": "Jordan is a first year Aeronautics and Astronautics student, who has been working with SUHPA since September 2018."
-    },
-    {
-      "id": 3,
-      "image": "./img/committee/tat.jpg",
-      "name": "Tatjana Mandil",
-      "job": "President",
-      "quote": "Tatjana is a second year Aeronautics and Astronautics student, with a focus on propulsion. She is one of the founding members of SUHPA"
-    },
-    {
-      "id": 4,
-      "image": "./img/committee/charles.jpg",
-      "name": "Charles Dhenin",
-      "job": "Vice-President",
-      "quote": "Charles is a former engineering foundation year student, with a passion for aircraft design, additive manufacturing and propulsion. He joined SUHPA in September 2018"
     },
     {
       "id": 5,
       "image": "./img/committee/matt.jpg",
       "name": "Matt Edgeson",
-      "job": "Secretary",
+      "jobs": [{
+        "id" : 1,
+        "job" : "Secretary"
+      }],
       "quote": "Matt studies Aerospace engineering was part of the founding members of SUHPA. As well as being secretary, he has experience as drive-train leader and Social Sec."
+    },
+    {
+      "id": 10,
+      "image": "./img/committee/default-user.jpg",
+      "name": "Justine McAvoy",
+      "jobs": [{
+        "id": 1,
+        "job": "Event and Social Manager"
+      }],
+      "quote": "Description"
+    },
+    {
+      "id": 1,
+      "image": "./img/committee/pran.jpg",
+      "name": "Prannay Roy",
+      "jobs": [{
+        "id": 1,
+        "job": "Treasurer"
+      },
+      {
+        "id": 2,
+        "job": "Wing Team Lead"
+      }],
+      "quote": "Prannay is a second year Aeronautics and Astronautics student. He has been in charge of the wing design since the creation of the society, and was recently made treasurer."
+    },
+    {
+      "id": 6,
+      "image": "./img/committee/titouan.jpg",
+      "name": "Titouan Barthelemy",
+      "jobs": [{
+        "id": 1,
+        "job": "Drivetrain Team Lead"
+      }],
+      "quote": "Titouan is a first year Aeronautics and Astronautics student, and is a big fan of all things related to bikes. He has been with SUHPA since September 2018"
+    },
+    {
+      "id": 7,
+      "image": "./img/committee/default-user.jpg",
+      "name": "Tom Andrews",
+      "jobs": [{
+        "id": 1,
+        "job": "Drivetrain Team Vice-Lead"
+      }],
+      "quote": "Description"
+    },
+    {
+      "id": 8,
+      "image": "./img/committee/default-user.jpg",
+      "name": "Samuel Pearson",
+      "jobs": [{
+        "id": 1,
+        "job": "Stability Team Lead"
+      }],
+      "quote": "Samuel is a aeronautics and astronautics engineering student with a love for aerospace, particularly the design of high lift devices and supersonic flight. He joined the SUHPA stability team in 2018."
+    },
+    {
+      "id": 9,
+      "image": "./img/committee/bob.jpg",
+      "name": "Robert Oxford Pope",
+      "jobs": [{
+        "id": 1,
+        "job": "Integration Team Lead"
+      }],
+      "quote": "Robert is a first year Aeronautics and Astronautics student. He has been working closely with the Solent Sky Museum on a flight simulator to train future HPA pilots."
     }]
   }
   render(){
--- src/Committee.js
+++ src/Committee.js
@@ -1,23 +1,71 @@
 import React from 'react';
+import Slider from 'react-slick';
 import './Committee.css'
-
 const Committee = ({committee}) => {
     const committeeList = committee.map(member => {
+        const jobList = member.jobs.map(job => {
+            return job.id > 0 ? (
+                <h4 key={job.id}>{job.job}</h4>
+            ) : null;
+        });
         return member.id > 0 ? (
-            <div className="member" key={member.id}>
+            <div key={member.id} className="member">
                 <img src={ member.image } alt={member.name}/>
                 <div className="member-content">
                     <h3>{member.name}</h3>
-                    <h4>{member.job}</h4>
+                    {
+                        jobList
+                    }
                     <p>{member.quote}</p>
                 </div>
             </div>
         ) : null;
     });
+    var settings = {
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 380,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
     return(
         <div className="committee">
             <h2>Our Committee</h2>
+            <Slider {...settings}>
             { committeeList }
+            </Slider>
         </div>
     );
 }
