8675c3212f2bb78ed870c4a1fc66f64a30939d5f
Jordan WIlliams <jordan@Jordans-MacBook-Pro.local>
2019-08-29 15:32:14 +0100

    Updates

--- precache-manifest.438b792b4222992a59d33217b14e609c.js
+++ precache-manifest.438b792b4222992a59d33217b14e609c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88db1ac7b343afbc73075c81ff6ca1a9",
+    "revision": "2b36c7966fb9690112b412c3971511d2",
     "url": "/suhpa-dev/index.html"
   },
   {
-    "revision": "0ccd6ea69a252435262b",
-    "url": "/suhpa-dev/static/css/main.0ae20ba8.chunk.css"
+    "revision": "61db108e55697a772515",
+    "url": "/suhpa-dev/static/css/main.ca65d772.chunk.css"
   },
   {
     "revision": "b3c920aa3e7d4c3789f1",
     "url": "/suhpa-dev/static/js/2.13f4209e.chunk.js"
   },
   {
-    "revision": "0ccd6ea69a252435262b",
-    "url": "/suhpa-dev/static/js/main.b8660cc9.chunk.js"
+    "revision": "61db108e55697a772515",
+    "url": "/suhpa-dev/static/js/main.adff3def.chunk.js"
   },
   {
     "revision": "d22e360a329d0dd35acb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suhpa-dev/precache-manifest.937a93db5605b93c8f351dca474e8b21.js"
+  "/suhpa-dev/precache-manifest.438b792b4222992a59d33217b14e609c.js"
 );
 
 self.addEventListener('message', (event) => {
